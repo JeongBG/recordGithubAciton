59c59a7092ac59e2b1c9bf0aff63e42c20c9d718
JeongBG <117093635+JeongBG@users.noreply.github.com>
2022-11-07 13:34:30 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print('Hello,,,, World !!!!!')
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://ridibooks.com/category/new-releases/2200'
+res = requests.get(url, encording = 'utf-8')
+soup = BeautifulSoup(res.text)
+
+new_arrivals = soup.select('.title_text')
+for no, book in enumerate(new_arrivals, 1):
+  print(no, book.text.strip())
