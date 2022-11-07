c3d9cd90af68221258de3181a4dfc9c58e901f68
JeongBG <117093635+JeongBG@users.noreply.github.com>
2022-11-07 13:41:40 +0900

    Update test.py

--- test.py
+++ test.py
@@ -7,4 +7,4 @@ soup = BeautifulSoup(res.text)
 
 new_arrivals = soup.select('.title_text')
 for no, book in enumerate(new_arrivals, 1):
-  print(no, book.text.strip())
+  print(no, book.text.strip()) # strip은 앞,뒤 공백을 없애고 글자만 가지고 오는 것.
